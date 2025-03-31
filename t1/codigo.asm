2e92601d8f992fce4e8c5978e72242335beb710e
Andrieli <andrieliluci@gmail.com>
2025-03-31 16:03:26 -0300

    [t1] tentando resolver problema de jump

--- t1/codigo.asm
+++ t1/codigo.asm
@@ -4,14 +4,12 @@
     sub r3, r3  ; Zera R3 
     addi 3
     slr r0, r0
-    addi 4
+    addi 2
     add r2, r0
     sub r0, r0
-    addi 5
-    addi 5
-    slr r0, r3 
-    slr r0, r3
-    add r1, r0 ; posição de r1 na memória
+    addi 4
+    slr r0, r0
+    add r1, r0  ; posição de r1 na memória
     sub r0, r0
     addi 5
     addi 5
@@ -19,10 +17,12 @@
 loop_R: sub r0, r0
     st r0, r1
     addi 1
-    add r1, r0
-    st r3, r0
-    addi -1
-    st r0, r3
+    add r1, r0   ; avança r1
+    sub r0, r0   ; r0 = 0 
+    add r0, r3   ; r0 = 10
+    addi -1      ; r0 = 9
+    sub r3, r3   ; r3 = 0
+    add r3, r0   ; r3 = 9
     brzr r0, r2
     ji loop_R
 ji end_R
