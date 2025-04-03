e208fcbc6c32052fab394c169ffcba5888368346
Andrieli <andrieliluci@gmail.com>
2025-04-03 08:24:37 -0300

    [t1] loop A refatorado

--- t1/teste2.asm
+++
@@ -1,7 +0,0 @@
-addi -1
-add r2, r0
-sub r0, r0
-addi 3
-slr r0, r0
-st r2, r0
-ebreak
---
+++ t1/vetorA.asm
@@ -0,0 +1,63 @@
+; -- definindo nº da instrução do branch (loop_A) --
+addi 3      ; r0 = 2
+slr r0, r0
+addi 7
+addi 7
+addi 7
+addi 6
+add r2, r0  ; r2 = 2
+sub r0, r0  ; zera r0
+addi -1     ; r0 = -1 (255)
+st r2, r0   ; guarda 2 na posição 255
+sub r2, r2  ; zera r2
+
+; -- posição vetor A -- 
+sub r0, r0  ; zera r0
+addi 4      ; r0 = 4
+slr r0, r0
+addi 5
+addi 5
+add r1, r0  ; r1 = 74
+
+sub r0, r0
+addi 1
+add r2, r0
+
+sub r0, r0  ; zera r0
+addi 5      ; r0 = 10
+addi 5
+add r3, r0  ; r3 = 10 (auxiliar do r0)
+sub r0, r0
+
+loop_A:         slr r0, r2   ; multiplica valor atual de r0 por 2
+                st r0, r1
+                sub r0, r0
+                addi 1
+                sub r1, r0   ; avança r1
+                sub r0, r0   ; r0 = 0
+                addi -1
+                ji meio_loop_A1
+
+meio_loop_A4:   ji loop_A
+
+meio_loop_A1:   ld r2, r0    ; R[r2] = M[R[r0]] -> 44
+                sub r0, r0
+                add r0, r3   ; r0 = r3
+                addi -1      ; r0 = r0 - 1
+                sub r3, r3   ; r3 = 0
+                add r3, r0   ; r3 = r0
+                ji meio_loop_A2
+
+meio_loop_A3:   ji meio_loop_A4
+
+meio_loop_A2:   brzr r0, r2
+                sub r0, r0
+                sub r2, r2
+                addi 1
+                add r2, r0
+                sub r0, r0
+                add r0, r3
+
+                ji meio_loop_A3
+ji end_loop_A
+end_loop_A: ebreak
