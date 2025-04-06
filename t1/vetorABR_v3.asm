d3f46f80e75c63ff7257b5268a6590e51e86a2b7
Andrieli <andrieliluci@gmail.com>
2025-04-06 20:55:36 -0300

    [t1] corrigir erro do loop R

--- t1/vetorABR_v3.asm
+++ t1/vetorABR_v3.asm
@@ -1,43 +1,45 @@
-; -- definindo nº da instrução do branch (loop A) --
+; -- definindo nº da instrução do branch (loop vetor A e B) --
 addi 4      ; r0 = 4
-slr r0, r0  ; r0 = (4 * 2^4) = 64
-addi 1     ; r0 = 62
-add r2, r0  ; r2 = 62
+slr r0, r0  ; r0 = (4 * 2^4) = 66
+addi 2      ; r0 = 66
+add r2, r0  ; r2 = 66
 
-add r1, r2
-sub r0, r0
-addi 1
-srr r1, r0
-addi 5
-add r1, r0
-add r1, r2
+; -- definindo nº da instrução do branch (loop vetor R) --
+add r1, r2  ; r1 = 66
+sub r0, r0  ; zera r0
+addi 1      ; r0 = 1
+srr r1, r0  ; r1 = (66 % 2^1) = 33
+addi 7      ; r0 = 9
+addi 1      ; r0 = 9
+add r1, r0  ; r1 = 33 + 9 = 42
+add r1, r2  ; r1 = 42 + 66 = 108
 
-; -- nº da iteração (ver se vale a pena) --
+; -- tamanho do vetor --
 sub r0, r0  ; zera r0
 addi 5      ; r0 = 10
 addi 5      ; r0 = 5
 add r3, r0  ; r3 = 10
 sub r0, r0  ; zera r0
 
-; -- guardando na memória valor de salto (loop AB) --
+; -- guardando na memória valor de salto (loop vetores A e B) --
 addi 5      ; r0 = 5 
 slr r0, r0  ; r0 = (5 * 2^5) = 160
-st r2, r0   ; guarda Y na posição 160
+st r2, r0   ; guarda 66 na posição 160
 
 ; -- guardando na memória valor de salto (loop R) --
 addi 1      ; r0 = 161
-st r1, r0   ; guarda X na posição 161
+st r1, r0   ; guarda 108 na posição 161
 
-; -- guardando nº da iteração (loop AB e R) --
+; -- guardando tamanho do vetor (para os loops A/B e R) --
 addi 1      ; r0 = 66
-st r3, r0   ; guarda 10 na posição 66
+st r3, r0   ; guarda 10 na posição 162
 addi 1      ; r0 = 67
-st r3, r0   ; guarda 10 na posição 67
+st r3, r0   ; guarda 9 na posição 163
 
 ; -- posição inicial do vetor -- 
-sub r1, r1
-addi 5      ; r0 = 72
-add r1, r0  ; r1 = 72
+sub r1, r1  ; zera r1
+addi 5      ; r0 = 168
+add r1, r0  ; r1 = 168
 
 ; -- valor auxiliar (vale 1) --
 sub r2, r2  ; zera r2
@@ -52,14 +54,14 @@ loop_AB:        st r3, r1   ; M[r1] = r3 (A)
                 sub r0, r0  ; zera r0
                 addi 5      ; r0 = 5
                 addi 5      ; r0 = 5
-                add r1, r0
+                add r1, r0  ; avança r1 em 10 posições
                 st r3, r1   ; M[r1] = r3 (B)
                 ji meio_loop_AB1
 
 meio_loop_AB6:  ji loop_AB
 
-meio_loop_AB1:  sub r1, r0
-                addi -5
+meio_loop_AB1:  sub r1, r0  ; retorna r1 em 10 posições
+                addi -5     ; r0 = 5
                 slr r0, r0  ; r0 = (5 * 2^5) = 160
                 addi 2      ; r0 = 162
                 ld r3, r0   ; r3 = r0 (10)
@@ -71,7 +73,7 @@ meio_loop_AB5:  ji meio_loop_AB6
 meio_loop_AB2:  st r3, r0   ; guarda r0-- na posição 162
                 addi -2     ; r0 = 160
                 ld r2, r0   ; r2 = M[r0]
-                brzr r3, r2 ; se r0 = 0, vai para instrução 65
+                brzr r3, r2 ; se r0 = 0, vai para instrução 66
                 sub r0, r0  ; zera r0
                 sub r2, r2  ; zero r2
                 
@@ -94,50 +96,50 @@ end_loop_AB:    sub r0, r0  ; zera R0
                 addi -4     ; r0 = 5
                 add r1, r0  ; r1 = 168
 
-loop_soma_R:    ld r2, r1  ; r2 = M[r1]
-                sub r0, r0 ; zera r0
-                addi 5     ; r0 = 5
-                addi 5     ; r0 = 10
-                add r1, r0 ; r1 = 178
-                ld r3, r1  ; r3 = M[r0]
+loop_soma_R:	sub r0, r0  ; zera r0
+		        sub r2, r2  ; zera r2
+                addi 5      ; r0 = 5
+                slr r0, r0  ; r0 = (5 * 2^5) = 160
+                addi 1      ; r0 = 161
                 ji loop_soma_R1
-
+                
 loop_soma_R8:   ji loop_soma_R
-            
-loop_soma_R1:   add r3, r2  ; r3 = r3 + r2 (R = A + B)
-                add r1, r0  ; r1 = 188 (vetor R)
-                st r3, r1   ; M[r1] = r3
-                sub r1, r0  ; r1 = 178 (vetor B)
-                sub r1, r0  ; r1 = 168 (vetor A)
+                
+loop_soma_R1:   ld r2, r0   ; r2 = M[r0]
+                addi 2      ; r0 = 163
+                ld r3, r0   ; r3 = r0 (10)
+                brzr r3, r2 ; se r3 = 0, vai para instrução 108
+                sub r2, r2  ; zera r2
+                add r2, r0  ; r2 = 163
                 ji loop_soma_R2
                 
 loop_soma_R7:   ji loop_soma_R8
-
+                
 loop_soma_R2:   sub r0, r0  ; zera r0
-                sub r2, r2  ; zera r2
                 addi 1      ; r0 = 1
-                add r2, r0  ; r2 = 1
+                sub r3, r0  ; r3 = r3 - 1
+                st r3, r2   ; guarda r3-- na posição 163
+                ld r2, r1   ; r2 = M[r1]
                 addi 4      ; r0 = 5
                 ji loop_soma_R3
 
 loop_soma_R6:   ji loop_soma_R7
-
-loop_soma_R3:   slr r0, r0  ; r0 = (5 * 2^5) = 160
-                addi 3      ; r0 = 163
-                ld r3, r0   ; r3 = r0 (10)
-                sub r3, r2  ; r3 = r3 - 1
+            
+loop_soma_R3:   addi 5      ; r0 = 10
+                add r1, r0  ; r1 = 178
+                ld r3, r1   ; r3 = M[r0]
+                add r3, r2  ; r3 = r3 + r2 (R = A + B)
+                add r1, r0  ; r1 = 188 (vetor R)
+                st r3, r1   ; M[r1] = r3
                 ji loop_soma_R4
-
+                
 loop_soma_R5:   ji loop_soma_R6
 
-loop_soma_R4:   st r3, r0   ; guarda r0-- na posição 163
-                addi -2     ; r0 = 161
-                ld r2, r0   ; r2 = M[r0]
-                brzr r3, r2 ; se r0 = 0, vai para instrução X
+loop_soma_R4:   sub r1, r0  ; r1 = 178 (vetor B)
+                sub r1, r0  ; r1 = 168 (vetor A)
                 sub r0, r0  ; zera r0
                 addi 1      ; r0 = 1
                 add r1, r0  ; aumenta r1 em 1 posição
                 ji loop_soma_R5
-
-ji end_soma_R
+                
 end_soma_R: ebreak
